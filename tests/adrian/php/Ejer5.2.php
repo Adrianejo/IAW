71c80f41264ee60a2a948b5950040deacf819dba
Adrian Menaza <adrianmenaza@gmail.com>
2020-10-29 20:41:32 +0100

    hecho

---
+++ tests/adrian/php/Ejer5.2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title>Problema</title>
+</head>
+<body>
+  <?php
+  $dia = 30; 
+  $sueldo = 1050.67; 
+  $nombre = "Jose Manue";
+  $exite = false; 
+  echo "Variable integer:";
+  echo $dia;
+  echo "<br>";
+  echo "Variable double:";
+  echo $sueldo;
+  echo "<br>";
+  echo "Variable string:";
+  echo $nombre;
+  echo "<br>";
+  echo "Variable boolean:";
+  echo $exite;
+  ?>
+</body>
+</html>
