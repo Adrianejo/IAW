bb58ff87170adf26a286bf6fae8d40373c1a01ff
Adrian Menaza <adrianmenaza@gmail.com>
2020-10-29 20:54:21 +0100

    acabado

---
+++ tests/adrian/php/Ejer6.2.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Problema6</title>
+</head>
+<body>
+    <?php
+    $multa1 = 600;
+    $multa2 = 1200;
+    $multa3 = 20000;
+    echo "La multa podra oscilar entre $multa1, $multa2, $multa3 €." ;
+    ?>
+</body>
+</html>
