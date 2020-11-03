9c25a237b8089fb85f05297512531f4fcd4c6217
Adrianejo <you@example.com>
2020-11-03 19:51:05 +0100

    uu

---
+++ tests/adrian/presentacion-personal.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title>Presentacion Personal</title>
+</head>
+
+<body>
+
+  <?php
+  $nombre = "adrian";
+  $apellido = "menaza";
+  $edad = "20";
+  $foto_url = "<img src=https://fotofigaredo.files.wordpress.com/2015/01/foto-carnet.jpg>";
+
+  echo "Nombre: $nombre"
+  echo "Apellido: $apellido"
+  echo "Edad: $edad"
+  echo "Foto: $foto_url"
+  
+  ?>
+</body>
+
+</html>
