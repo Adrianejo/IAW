302a8eb5d62b409438c584bc2ae6386345369176
Adrianejo <you@example.com>
2020-11-04 19:44:21 +0100

    ss

---
+++ tests/adrian/array/deberes2.php
@@ -0,0 +1,20 @@
+<?php
+//ARRAY EXAMPLES
+$numbers        = array(1, 2, 3, 5, 10, 20);
+
+
+$profesores = array(
+    'Javier',
+    'MariSol',
+    'Florindo',
+    'Fidel',
+    'Roberto'
+);
+$asignaturas = array(
+    'Base de datos',
+    'Servicios de red e internet',
+    'Sistemas operativos',
+    'Implantacion',
+    'Segurida y alta disponibilidad',
+    "Empresa"
+);
