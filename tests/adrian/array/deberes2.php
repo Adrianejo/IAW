e04ed0f89294bd72959167615cefd17d2308071c
Adrianejo <you@example.com>
2020-11-04 19:53:07 +0100

    ddd

--- tests/adrian/array/deberes2.php
+++ tests/adrian/array/deberes2.php
@@ -20,3 +20,52 @@ $asignaturas = array(
 );
 echo "<pre>";
 echo "<p> Don o Doña<b>".getRandomArray($profesores). "</b> da clase de <b>".getRandomArray($asignaturas)."</b></p>";
+function getRandomArray($array){
+    return $array[rand ( 0 , count ( $array ) - 1 )];
+}
+
+echo "<p>El jugador asignado es ".$players[rand ( 0 , sizeof($players) -1 ) ]."</p>";
+
+
+$people = [];
+
+$person = [
+    'Dino Cajic',
+    32,
+    '111-11-1111', // Don't store SSN like this
+    'dinocajic@gmail.com',
+    42.5, // favorite decimal
+    true // is awesome
+];
+
+
+$person[6] = "Author";
+$person[] = "An Illustrative Introduction to Algorithms";
+
+$person[999] = "https://medium.com/@dinocajic";
+$person[] = "donde estoy?";
+$person[1] = 33;
+
+$profesores = array(
+    'Javier',
+    'MariSol',
+    'Florindo',
+    'Fidel',
+    'Roberto'
+);
+$asignaturas = array(
+    'Base de datos',
+    'Servicios de red e internet',
+    'Sistemas operativos',
+    'Implantacion',
+    'Segurida y alta disponibilidad',
+    "Empresa"
+);
+
+foreach ($players as $key => $value) {
+    echo "<p>El jugador ".$value." pertenece al ".$teams[$key]."</p>";
+}
+
+for ($i= 0; $i<count($players); $i++) {
+    echo "<p>El jugador ".$players[$i]." pertenece al ".$teams[$i]."</p>";
+}
