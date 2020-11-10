ba5698f0570718ae427d99b69100b27870b5c1c3
Adrianejo <you@example.com>
2020-11-10 16:31:40 +0100

    33

---
+++ tests/adrian/p-aiw-1/ejer3.php
@@ -0,0 +1,15 @@
+<?php
+
+    $fecha  = date ("j/n/Y H:i");
+    print ("$fecha") .  "<BR>";
+    
+ /* Tabla */   
+
+    $dia = date ("j");
+    $mes = date ("n");
+    $anio = date ("Y");
+
+    Print ("Hoy es " . $dia . " del " . $mes . " del año " . $anio . "<BR>");
+
+
+?>
