c1675e173c77b6ed2dd651bd61c6d12e7f6d7999
Adrianejo <you@example.com>
2020-12-03 19:51:19 +0100

    cartass

--- tests/adrian/ejer_clase_03-12/carta-alta.php
+++ tests/adrian/ejer_clase_03-12/carta-alta.php
@@ -5,7 +5,7 @@
  *
  * El valor más alto se puede obtener con la función max().
  *
- * @author Escriba aquí su nombre
+ * @adrian Escriba aquí su nombre
  *
  */
 ?>
@@ -23,19 +23,31 @@
 </head>
 
 <body>
-  <h1>La carta más alta</h1>
+<h1>La carta más alta</h1>
+<p>Actualice la página para mostrar un nuevo trío de cartas.</p>
 
-  <p>Actualice la página para mostrar un nuevo trío de cartas.</p>
-  
+  <?php
+  $carta1 = rand(1,10);
+  $carta2 = rand(1,10);
+  $carta3 = rand(1,10);
 
-<?php
+  $carta_alta = max($carta1, $carta2, $carta3);
 
-print "  <p class=\"aviso\">La carta mas alta</p>\n";
+  $imgsrc1 = "img/c".$carta1.".svg";
+  $imgsrc2 = "img/c".$carta2.".svg";
+  $imgsrc3 = "img/c".$carta3.".svg";
+  $imgsrcAlta = "img/c".$carta_alta.".svg";
 
-?>
+  ?>
+    <img class="carta" src="<?php echo $imgsrc1; ?>">
+    <img class="carta" src="<?php echo $imgsrc2; ?>">
+    <img class="carta" src="<?php echo $imgsrc3; ?>">
+    <br></br>
+    <p>La carta mas alta es:</p>
+    <img class="carta" src="<?php echo $imgsrcAlta; ?>">
 
   <footer>
-    <p>Escriba aquí su nombre</p>
+    <p>Adrian</p>
   </footer>
 </body>
 </html>
