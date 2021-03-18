480468e28117a26c89762da500c3b0bf07931561
Adrian Menaza <adrianmenaza@gmail.com>
2021-03-18 13:58:41 +0100

    muestra cartas hecho

--- recuperacion/04-03-21/11-03-21/1.2- muestra-cartas/muestra-cartas.php
+++ recuperacion/04-03-21/11-03-21/1.2- muestra-cartas/muestra-cartas.php
@@ -14,28 +14,46 @@
 </head>
 
 <body>
-  <h1>Muestra dados</h1>
+  <h1>Muestra Cartas</h1>
 
 <?php
-
+$array=array(
+    "p",
+    "c",
+    "d",
+    "t",
+);
+$palo = $array [rand(0,3)];
 
 $cantidad= rand(3,12);
-$total=0;
+$p=0;
+$c=0;
+$d=0;
+$t=0;
 
 for ($i=0; $i <$cantidad ; $i++) { 
-    $dado= rand(1,6);
-    echo  "<img src='img/".$dado.".svg' >";
-    $total= $dado + $total;
+  $palo = $array [rand(0,3)];
+  $cantidad1= rand(1,10);
+  if ($palo == "p") {
+    $p++;
+  }else if ($palo == $c) {
+    $c++;
+  } else if ($palo == $d) {
+    $d++;
+  } else if ($palo == $t) {
+    $t++;
+  }
+  
+  echo    "<img src='img/".$palo.$cantidad1.".svg' width='140' height='140'>";
 }
-
-echo "<p>He tirado el total de ".$cantidad." dados</p>";
-
-echo "<p>El total de dados es: ".$total."</p>";
-
-
+echo "<ol>";
+echo "<li>Hay ".$p." cartas de picas</li>";
+echo "<li>Hay ".$c." cartas de corazones</li>";
+echo "<li>Hay ".$d." cartas de diamantes</li>";
+echo "<li>Hay ".$t." cartas de trevoles</li>";
+echo "</ol>";
 ?>
-
-  <footer>
+<footer>
     <p>Adrian Menaza</p>
   </footer>
 </body>
