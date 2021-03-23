f5cb14b94eca5d667d9b79f632a572873d72252a
Adrian Menaza <adrianmenaza@gmail.com>
2021-03-23 14:19:24 +0100

    matriz1

---
+++ recuperacion/04-03-21/sf-matrices-1-plantillas/matrices-1-01.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Matrices (1) 1 - matrices-1-01.php
+ *
+ * @author Escriba aquí su nombre
+ *
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Dado.
+    Matrices (1). Sin formularios.
+    Escriba aquí su nombre
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+  <h1>Dado</h1>
+
+  <p>Actualice la página para mostrar una nueva tirada.</p>
+
+<?php
+
+$dado = rand(1,6);
+$numero = ["", "uno", "dos", "tres", "cuatro", "cinco" ,"seis"];
+
+echo "<img src='img/".$dado.".svg' >";
+
+echo "Has sacado un ".$numero[$dado]."";
+
+?>
+
+  <footer>
+    <p>Adrian Menaza</p>
+  </footer>
+</body>
+</html>
