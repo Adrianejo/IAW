8b5806e254f250a888dffcc9348f6c760c65f2f6
Adrianejo <you@example.com>
2021-05-12 17:04:59 +0200

    y

--- recuperacion/04-03-21/examen-200227-plantillas/cs-trio-cartas/trio-cartas-2.php
+++ recuperacion/04-03-21/examen-200227-plantillas/cs-trio-cartas/trio-cartas-2.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 
-if ($_POST['cambia'] = ['1']) {
+if ($_POST['cambia'] = '1') {
     $_SESSION['carta1'] = rand(1,10);
 }
 
-if ($_POST['cambia'] = ['2']) {
+if ($_POST['cambia'] = '2') {
     $_SESSION['carta2'] = rand(1,10);
 }
-if ($_POST['cambia'] = ['3']) {
+if ($_POST['cambia'] = '3') {
     $_SESSION['carta3'] = rand(1,10);
 }
 
